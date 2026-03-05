c9a7a7094710030f36a536e63aab16261f4f652a
星必尘Sguan <3464647102@qq.com>
2026-03-06 02:51:51 +0800

    SguanFOC库v3.0.0

--- SguanFOC库v3.0.0(有感foc，浮点运算，初阶)/UserData_Correspond.h
+++ SguanFOC库v3.0.0(有感foc，浮点运算，初阶)/UserData_Correspond.h
@@ -1,8 +1,6 @@
 #ifndef __CORRESPOND_H
 #define __CORRESPOND_H
 /* 电机控制User用户设置·通信协议设定 */
-#include "main.h"
-extern UART_HandleTypeDef huart1;
 
 
 /* ================= 驱动代码(驱动层) ================= */
