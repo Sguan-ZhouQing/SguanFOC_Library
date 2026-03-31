ace8e61050620d9ee84769f1a6c9c3995f92817e
星必尘Sguan <3464647102@qq.com>
2026-04-01 04:27:21 +0800

    v3.1.0(书写中，先行预告)

---
+++ SguanFOC库v3.1.0(无感浮点库...书写中)/Sguan_EKF.h
@@ -0,0 +1,9 @@
+#ifndef __SGUAN_EKF_H
+#define __SGUAN_EKF_H
+
+/* 外部函数声明 */
+#include "Sguan_Config.h"
+
+
+
+#endif // SGUAN_EKF_H
---
+++ SguanFOC库v3.1.0(无感浮点库...书写中)/Sguan_SMO.h
@@ -0,0 +1,9 @@
+#ifndef __SGUAN_SMO_H
+#define __SGUAN_SMO_H
+
+/* 外部函数声明 */
+#include "Sguan_Config.h"
+
+
+
+#endif // SGUAN_SMO_H
