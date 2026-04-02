768e3d398d8697eb3f4b5e561555db1974704b2a
星必尘Sguan <3464647102@qq.com>
2026-04-03 02:00:42 +0800

    v3.1.0(书写中ing，先行预告)

---
+++ SguanFOC库v3.1.0(无感浮点库...writing)/Sguan_Bang.h
@@ -0,0 +1,8 @@
+#ifndef __SGUAN_BANG_H
+#define __SGUAN_BANG_H
+
+/* 外部函数声明 */
+#include "Sguan_Config.h"
+
+
+#endif // SGUAN_BANG_H
---
+++ SguanFOC库v3.1.0(无感浮点库...writing)/Sguan_DTC.h
@@ -0,0 +1,8 @@
+#ifndef __SGUAN_DTC_H
+#define __SGUAN_DTC_H
+
+/* 外部函数声明 */
+#include "Sguan_Config.h"
+
+
+#endif // SGUAN_DTC_H
